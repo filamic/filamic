b0c86951b35419f7e456c2d858ad342c752af9e6
christmex <christmex@yahoo.com>
2025-10-15 19:35:09 +0700

    Refactor UserFactory to use bcrypt for password hashing and remove unused properties; add User model tests for fillable attributes, hidden fields, and casts.

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use Illuminate\Support\Carbon;
+
+test('fillable', function () {
+    expect(User::factory()->make()->toArray())
+        ->toHaveKeys([
+            'name',
+            'email',
+        ]);
+});
+
+test('hidden', function () {
+    expect(
+        User::factory()
+            ->create([
+                'password' => 'password',
+                'remember_token' => 'token',
+            ])
+            ->toArray()
+    )
+        ->not->toHaveKeys([
+            'password',
+            'remember_token',
+        ]);
+});
+
+test('casts', function () {
+    expect(
+        User::factory()
+            ->create([
+                'password' => 'password',
+            ])
+    )
+        ->email_verified_at->toBeInstanceOf(Carbon::class)
+        ->password->toStartWith('$2y');
+});
