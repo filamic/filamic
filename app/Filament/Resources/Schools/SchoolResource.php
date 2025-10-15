d7895e82394d0822fe20c465d6a2c00278954e54
christmex <christmex@yahoo.com>
2025-10-15 21:02:40 +0700

    Update composer.json to include secondnetwork/blade-tabler-icons package and adjust navigation icon in SchoolResource to use 'tabler-school'; enhance Laravel boost guidelines with testing requirements.

--- app/Filament/Resources/Schools/SchoolResource.php
+++ app/Filament/Resources/Schools/SchoolResource.php
@@ -15,14 +15,13 @@ use App\Models\School;
 use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 
 class SchoolResource extends Resource
 {
     protected static ?string $model = School::class;
 
-    protected static string | BackedEnum | null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string | BackedEnum | null $navigationIcon = 'tabler-school';
 
     public static function table(Table $table): Table
     {
