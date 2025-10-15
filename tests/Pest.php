09375236b54cbae53b933edeaa2893dd1136301f
christmex <christmex@yahoo.com>
2025-10-15 19:18:59 +0700

    Run composer analyse and add pint.json

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,12 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 pest()->extend(Tests\TestCase::class)
  // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
+expect()->extend('toBeOne', fn () => $this->toBe(1));
 
 function something()
 {
