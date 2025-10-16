1ae2b1a5f79f7f2ed51a50e9d96d60c30a1d666c
christmex <christmex@yahoo.com>
2025-10-16 17:21:48 +0700

    Add test to AdminPanelTest

--- tests/Feature/Filament/Admin/AdminPanelTest.php
+++ tests/Feature/Filament/Admin/AdminPanelTest.php
@@ -1,3 +1,10 @@
 <?php
 
 declare(strict_types=1);
+
+test('authenticated users can access admin panel', function () {
+    $this->loginAdmin();
+
+    $this->get('/admin')
+        ->assertOk();
+});
