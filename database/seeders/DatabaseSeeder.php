2080a409751f7704229713217f98a0cba0f7db2d
christmex <christmex@yahoo.com>
2025-10-19 23:04:46 +0700

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\Classroom;
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\School;
-use App\Models\User;
+use App\Models\Subject;
+use App\Models\Classroom;
+use App\Models\SchoolYear;
+use App\Models\SubjectCategory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,17 +28,40 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('mantapjiwa00'),
         ]);
 
-        $this->createSchools();
         $this->createClassrooms();
-    }
-
-    public function createSchools(): void
-    {
-        School::factory(10)->create();
+        $this->createSchoolYear();
+        $this->createSubjectCategories();
+        $this->createSubjects();
     }
 
     public function createClassrooms(): void
     {
         Classroom::factory(10)->create();
+    }
+    
+    public function createSchoolYear(): void
+    {
+        SchoolYear::factory()->active()->create();
+    }
+
+    public function createSubjectCategories(): void
+    {
+        SubjectCategory::factory()
+            ->state([
+                'name' => 'General Subject',
+                'sort_order' => 1,
+            ])
+            ->create();
+    }
+
+    public function createSubjects(): void
+    {
+        Subject::factory()->state([
+            'subject_category_id' => SubjectCategory::first()->getRouteKey(),
+            'name' => 'Mathematics',
+            'sort_order' => 1,
+        ])->create();
+
+        Subject::factory(10)->create();
     }
 }
