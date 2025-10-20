8ce6304dbfecccc378d3b36240ed06b028eeeb59
christmex <christmex@yahoo.com>
2025-10-20 13:08:38 +0700

    Update migration

---
+++ database/migrations/2025_10_19_204110_create_teachings_table.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Subject;
+use App\Models\Teacher;
+use App\Models\Classroom;
+use App\Models\SchoolYear;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('teachings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Teacher::class)->constrained();
+            $table->foreignIdFor(Subject::class)->constrained();
+            $table->foreignIdFor(Classroom::class)->constrained();
+            $table->foreignIdFor(SchoolYear::class)->constrained();
+            $table->timestamps();
+        });
+    }
+};
