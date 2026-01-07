4c6e4e8f1bc57de336dbd606791e63c5029bb4cf
christmex <christmex@yahoo.com>
2026-01-07 15:07:50 +0700

    Add theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,23 +4,24 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
+use Filament\Pages\Dashboard;
 use Filament\Support\Enums\Width;
+use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
+use Filament\Support\Enums\Platform;
 use Filament\Widgets\FilamentInfoWidget;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
+use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Filament\Http\Middleware\AuthenticateSession;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -62,9 +63,19 @@ class AdminPanelProvider extends PanelProvider
             ->topbar(false)
             // ->topNavigation()
             ->maxContentWidth(Width::Full)
-            ->sidebarWidth('18rem')
-            ->brandLogo(asset('logo_filamic.svg'))
+            ->sidebarWidth('16rem')
+            ->brandLogo(asset('logo_basic_digital.svg'))
             ->font('Inter')
-            ->readOnlyRelationManagersOnResourceViewPagesByDefault(false);
+            ->readOnlyRelationManagersOnResourceViewPagesByDefault(false)
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->brandLogoHeight('1rem')
+            ->spa()
+            // ->sidebarCollapsibleOnDesktop()
+            ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
+            ->globalSearchFieldSuffix(fn (): ?string => match (Platform::detect()) {
+                Platform::Windows, Platform::Linux => 'CTRL+K',
+                Platform::Mac => '⌘K',
+                default => null,
+            });
     }
 }
